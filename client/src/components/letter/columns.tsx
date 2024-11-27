f4b1d908030c474d9777153875dd683b46152119
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-11-27 14:20:02 +0700

    add delete and toast without mutation 🗿

--- client/src/components/letter/columns.tsx
+++ client/src/components/letter/columns.tsx
@@ -14,6 +14,21 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ROUTES } from "@/types/routes";
+import { useMutation } from "react-query";
+import axios from "axios";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useToast } from "@/hooks/use-toast";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -47,30 +62,80 @@ export const columns: ColumnDef<Letter>[] = [
     header: "Aksi",
     cell: ({ row }) => {
       const letter = row.original;
+      const { toast } = useToast();
+
+      const deleteLetter = async () => {
+        await axios.delete(`${ROUTES.ARSIP}${letter.id}`);
+        toast({
+          title: "Berhasil",
+          description: "Surat berhasil dihapus",
+          className: "bg-standard text-white",
+        });
+        // const { data, isSuccess } = useMutation({
+        //   mutationFn: async () => {
+        //     await axios.delete(`${ROUTES.ARSIP}${letter.id}`);
+        //     console.log(data);
+
+        //     isSuccess &&
+        //       toast({
+        //         title: "Berhasil",
+        //         description: "Surat berhasil dihapus",
+        //         className: "bg-standard text-white",
+        //       });
+
+        //     // document.body.appendChild(link)
+        //     // link.click()
+        //     // link.remove()
+        //   },
+        // });
+      };
 
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Buka menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="bg-standard text-white">
-            <DropdownMenuLabel>Aksi</DropdownMenuLabel>
-            {/* <DropdownMenuItem
+        <AlertDialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Buka menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="bg-standard text-white">
+              <DropdownMenuLabel>Aksi</DropdownMenuLabel>
+              {/* <DropdownMenuItem
               onClick={() => navigator.clipboard.writeText(payment.nomor_surat)}
             >
               Copy payment ID
             </DropdownMenuItem> */}
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Hapus</DropdownMenuItem>
-            <DropdownMenuItem>
-              <a href={`${ROUTES.ARSIP_PDF}${letter.id}`}>Unduh</a>
-            </DropdownMenuItem>
-            <DropdownMenuItem>Lihat</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <AlertDialogTrigger>Hapus</AlertDialogTrigger>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <a href={`${ROUTES.ARSIP_PDF}${letter.id}`}>Unduh</a>
+              </DropdownMenuItem>
+              <DropdownMenuItem>Lihat</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <AlertDialogContent className="bg-standard text-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Surat "{letter.judul}" akan dihapus
+              </AlertDialogTitle>
+              <AlertDialogDescription className="text-white">
+                Surat yang dihapus tidak dapat dikembalikan. Apakah anda yakin
+                akan menghapus ?
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel className="text-black">
+                Kembali
+              </AlertDialogCancel>
+              <AlertDialogAction className="bg-red-500" onClick={deleteLetter}>
+                Hapus
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       );
     },
   },
